d23928993346481e0ca55815092c0d82817b8fe6
Max Fyodorov <m.v.fyodoroff@gmail.com>
2021-01-07 14:15:03 +0300

    uikit/cards is finished

--- src/components/uikit/form-elements/star-rating/star-rating.js
+++ src/components/uikit/form-elements/star-rating/star-rating.js
@@ -2,6 +2,10 @@ const rating = document.querySelectorAll('.rating')
 
 Array.from(rating).forEach(function(i) {
     const ratingItems = Array.from(i.children)
+    const ratng = i.closest('.rating')
+    const starsRated = parseInt(ratng.dataset.starsRated) + 1
+
+    addClss(ratingItems)
     
     i.addEventListener('click', function(e) {
         const ratingItem = e.target.closest('.rating-item')
@@ -28,50 +32,65 @@ Array.from(rating).forEach(function(i) {
         isRated(ratingItems)
     })
 
+    function addClss(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (starsRated === parseInt(arr[i].dataset.rate)) {
+                break
+            } else {
+                arr[i].classList.add('rated')
+                isRated(arr)
+            }
+        }
+    }
+
+    function removeClass(arr) {
+        for (let i = 0; i < arr.length; i++){
+            for (let j = 1; j < arguments.length; j++){
+                arr[i].classList.remove(arguments[j])
+            }
+        }
+    }
+    
+    function addClass(arr) {
+        for (let i = 0; i < arr.length; i++){
+            for (let j = 1; j < arguments.length; j++){
+                arr[i].classList.add(arguments[j])
+            }
+        }
+    }
+    
+    function mouseOverRatedClass(arr) {
+        for(let i = 0; i < arr.length; i++) {
+            if (arr[i].classList.contains('rated')) {
+                break
+            } else {
+                arr[i].classList.add('rated')
+            }
+        }
+    }
+    
+    function mouseOutRatedClass(arr) {
+        for(let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i].classList.contains('current-rated')) {
+                break
+            } else {
+                arr[i].classList.remove('rated')
+            }
+        }
+    }
+    
+    function isRated (arr) {
+        arr.forEach(i => {
+            if (i.classList.contains('rated')) {
+                i.children[0].children[1].style.display = 'block'
+            } else {
+                i.children[0].children[1].style.display = 'none'
+            }
+        })
+    }
 })
 
-function removeClass(arr) {
-    for (let i = 0; i < arr.length; i++){
-        for (let j = 1; j < arguments.length; j++){
-            arr[i].classList.remove(arguments[j])
-        }
-    }
-}
 
-function addClass(arr) {
-    for (let i = 0; i < arr.length; i++){
-        for (let j = 1; j < arguments.length; j++){
-            arr[i].classList.add(arguments[j])
-        }
-    }
-}
 
-function mouseOverRatedClass(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        if (arr[i].classList.contains('rated')) {
-            break
-        } else {
-            arr[i].classList.add('rated')
-        }
-    }
-}
 
-function mouseOutRatedClass(arr) {
-    for(let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i].classList.contains('current-rated')) {
-            break
-        } else {
-            arr[i].classList.remove('rated')
-        }
-    }
-}
 
-function isRated (arr) {
-    arr.forEach(i => {
-        if (i.classList.contains('rated')) {
-            i.children[0].children[1].style.display = 'block'
-        } else {
-            i.children[0].children[1].style.display = 'none'
-        }
-    })
-}
--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ import './components/uikit/form-elements/range-slider/range-slider';
 import './components/uikit/form-elements/dropdown/dropdown';
 import './components/uikit/cards/calendar/calendar';
 import './components/uikit/cards/card__search-room/card__search-room';
+import './components/uikit/cards/card__booking/card__booking';
+import './components/uikit/cards/card__login/card__login';
+
+
+
